86b3cc0410b3b1bd9edf95ee5e479da19da54167
Katie Picha <Katie_Picha1@baylor.edu>
2021-11-29 16:27:34 -0600

    Topics Page

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 def index(request):
     '''The home page for Learning Log.'''
     return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+
+    # the key is the variable used in the template file and the value is the variable used in the view function
+    context = {'topics':topics}
+
+    return render(request, 'MainApp/topics.html', context)

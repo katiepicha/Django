94005bcb0c7c2c8ecaaf4a904c5cf60a7a5bcf5b
Katie Picha <Katie_Picha1@baylor.edu>
2021-12-06 17:05:01 -0600

    Login required

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import TopicForm, EntryForm
 from .models import Topic, Entry
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     '''The home page for Learning Log.'''
     return render(request, 'MainApp/index.html')
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
 
@@ -15,6 +17,7 @@ def topics(request):
 
     return render(request, 'MainApp/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
@@ -23,6 +26,7 @@ def topic(request, topic_id):
 
     return render(request, 'MainApp/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm() # loading an empty form if get request
@@ -37,6 +41,7 @@ def new_topic(request):
     context = {'form': form} # dictionary that allows us to pass data into our template
     return render(request, 'MainApp/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id): # need to use the same variable from the URLs file (we use topic_id in the urls.py file)
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -54,6 +59,7 @@ def new_entry(request, topic_id): # need to use the same variable from the URLs 
     context = {'form': form, 'topic':topic} # dictionary that allows us to pass data into our template
     return render(request, 'MainApp/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     '''Edit an existing entry.'''
     entry = Entry.objects.get(id=entry_id)
